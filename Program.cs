e1e2581bb06923b26af02eadd82ce40c3dd5c290
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-16 14:00:32 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Prog_Fundamentals_Midterm_Ishak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
